40674877bdd19ae25e999a6f3a6d3b941f0b4947
Nora Posthumu <nora@posthumus.ch>
2025-04-09 17:43:44 +0200

    Add session based UI State

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,41 +1,62 @@
-/**
-* This function sets the open attribute of the element targeted in the location hash
-**/
-function openTarget() {
-    const hashes = location.hash.substring(1).split('#');
-    hashes.forEach(hash => {
-        if (hash) {
-            const target = document.getElementById(hash);
-            console.log(hash)
-            if (target) {
-                const details = target.closest('details');
-                if (details)
-                    details.open = true;
-            }
-        }
-    })
-}
-
-window.addEventListener('load', openTarget)
-window.addEventListener('hashchange', openTarget)
-
-function removeHashFromLocation(value) {
-    location.hash = location.hash.replaceAll('#' + value, '');
-}
-
-function addHashToLocation(value) {
-    removeHashFromLocation(value);
-    location.hash += '#' + value;
+function normalizePath(path) {
+    path = path.replace(/\.[^/.]+$/, ''); // Removes file extensions
+  
+    // Remove trailing slash (to ensure "page" and "page/" are the same)
+    return path.replace(/\/$/, '');
 }
 
 function handleAccordionToggle(event) {
-    if (event.newState === "open") {
-        addHashToLocation(event.target.id);
-    } else {
-        removeHashFromLocation(event.target.id);
-    }
+    updateUIState("accordionState", {[event.target.id]: event.newState})
 }
 
 Array.from(document.getElementsByClassName('accordion__element')).forEach(accordionToggle => {
     accordionToggle.addEventListener('toggle', handleAccordionToggle);
 })
+
+function getUIState() {
+    const globalUIState = JSON.parse(sessionStorage.getItem('uiState') || '{}');
+    return globalUIState[normalizePath(window.location.pathname)] || {};
+}
+
+function updateUIState(state, update) {
+    const globalUIState = JSON.parse(sessionStorage.getItem('uiState') || '{}');
+    let localUIState = getUIState();
+    localUIState = {...localUIState, [state]: {...localUIState[state], ...update}};
+    sessionStorage.setItem('uiState', JSON.stringify({
+        ...globalUIState,
+        ...{[normalizePath(window.location.pathname)]: localUIState}
+    }));
+}
+
+function handleLoad() {
+    localUIState = getUIState();
+
+    // apply accordionState
+    Object.entries(localUIState.accordionState || {}).forEach(([key, value]) => {
+        target = document.getElementById(key);
+        if(target) {
+            target.open = value == "open";
+        }
+    })
+
+    // apply scrollState
+    scrollPos = localUIState?.scrollState?.scrollY
+    if (scrollPos !== null) window.scrollTo(0, scrollPos);
+
+    // automatically show details of element targeted by hash
+    const hash = location.hash.substring(1);
+    if (hash) {
+        const target = document.getElementById(hash);
+        if (target) {
+            const details = target.closest('details');
+            if (details)
+                details.open = true;
+        }
+    }
+}
+
+window.addEventListener('beforeunload', () => {
+    updateUIState("scrollState", {"scrollY": window.scrollY})
+});
+
+window.addEventListener('load', handleLoad);
